b3fc4416e30226950bcdeb111d8b804a3ce1c0f3
Jacques Van Blokland <jacquesvb@live.com>
2024-03-12 15:05:30 -0700

    Add light-dark mode

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link';
+import { ModeToggle } from '@/components/ModeToggle';
 
 export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
       <Link href="/" className="font-bold text-3xl">
-        JacquesVB<span className="text-blue-500">Blog</span>
+        JacquesVB<span className="text-primary">Blog</span>
       </Link>
+      <ModeToggle />
     </nav>
   );
 }
