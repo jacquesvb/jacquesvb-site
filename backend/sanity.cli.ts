09783d4af0f3be71d4252b7a9bdd5ee946ded4cd
Jacques Van Blokland <jacquesvb@live.com>
2024-03-12 14:45:47 -0700

    Initial commit

---
+++ backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'pwmrke8w',
+    dataset: 'production'
+  }
+})
---
+++ backend/schemaTypes/blogType.ts
@@ -0,0 +1,21 @@
+import {defineField, defineType} from 'sanity'
+
+export const blogType = defineType({
+  name: 'blog',
+  type: 'document',
+  title: 'Blog',
+  fields: [
+    defineField({ name: 'title', type: 'string', title: 'Title of blog article' }),
+    defineField({ 
+      name: 'slug', 
+      type: 'slug', 
+      title: 'Slug of your blog article',
+      options: {
+        source: 'title'
+      }
+     }),
+    defineField({ name: 'titleImage', type: 'image', title: 'Title Image' }),
+    defineField({ name: 'smallDescription', type: 'text', title: 'Small Description' }),
+    defineField({ name: 'content', type: 'array', title: 'Content', of: [{ type: 'block' }] })
+  ]
+})
